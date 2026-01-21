4112086e7a2441d26faa7375a88ab3642be4b09b
Hugo <martin.hugo2711@gmail.com>
2026-01-21 15:27:26 +0100

    Ajout de documentation pour le TP3

--- TP3/main.py
+++ TP3/main.py
@@ -2,14 +2,19 @@ import search_motor as sm
 
 
 def get_list_of_queries():
+    # setting the queries
     queries = [
         "box of chocolate orange",
         "italy leather",
+        "no results",
+        "Hiking Boots for Outdoor Adventures",
+        "Chololate from france"
     ]
     return queries
 
 
 def main():
+    # loading all documents
     rearranged_products = sm.read_jsonl("TP3/rearranged_products.jsonl")
     title_index = sm.read_json("TP3/title_index.json")
     description_index = sm.read_json("TP3/description_index.json")
@@ -17,8 +22,10 @@ def main():
     origin_synonyms = sm.read_json("TP3/origin_synonyms.json")
     brand_index = sm.read_json("TP3/brand_index.json")
 
+    # getting queries
     queries = get_list_of_queries()
 
+    # computing the results for each query
     for query in queries:
         print(query, ":")
         ranking_result = sm.provide_ranking_results(query,
--- TP3/search_motor.py
+++ TP3/search_motor.py
@@ -10,16 +10,19 @@ import numpy as np
 # JSON READ AND SAVE
 
 def read_jsonl(filename):
+    # reading jsonl file
     with open(filename, 'r', encoding='utf-8') as f:
         return [json.loads(line) for line in f]
 
 
 def read_json(filename):
+    # reading json file
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
 def save_to_json(data, filename):
+    # saving json file
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -33,8 +36,11 @@ def download_token():
 
 
 def tokenize(stop_words, text):
+    # tokenize the text
     tokens = word_tokenize(text.lower())
+    # removing stop words
     current_tokenized = [word for word in tokens if word not in stop_words]
+    # removing punctuation
     current_tokenized = [re.sub(r'[^\w\s]', '', token) for token in
                          current_tokenized if re.sub(r'[^\w\s]', '', token)]
     return current_tokenized
@@ -44,19 +50,24 @@ def replace_with_synonyms(request_tokens, synonyms_index):
     new_tokens = []
     for token in request_tokens:
         replaced = False
+        # looking for token in synonyms lists
         for original, synonyms in synonyms_index.items():
             if token in synonyms:
                 new_tokens.append(original)
                 replaced = True
                 break
+        # if no synonym if found then we keep the token
         if not replaced:
             new_tokens.append(token)
     return new_tokens
 
 
 def transform_request(request, list_of_synonyms_indexes):
+    # loading stopwords
     stop_words = set(stopwords.words('english'))
+    # tokenizing the query
     request_tokens = tokenize(stop_words, request)
+    # finding synonyms
     for synonyms_index in list_of_synonyms_indexes:
         current_tokens = replace_with_synonyms(request_tokens, synonyms_index)
     return current_tokens
@@ -74,6 +85,7 @@ def find_token_from_index(token, index):
 
 def find_token_from_all_indexes(token, indexes):
     result = []
+    # looking for token in all indexes
     for index in indexes:
         current_result = find_token_from_index(token, index)
         result.extend(current_result)
@@ -82,6 +94,7 @@ def find_token_from_all_indexes(token, indexes):
 
 def find_tokens_from_index(tokens, index):
     result = []
+    # looking for all tokens in an index
     for token in tokens:
         current_result = find_token_from_index(token, index)
         result.extend(current_result)
@@ -91,10 +104,13 @@ def find_tokens_from_index(tokens, index):
 def find_tokens_from_all_indexes(tokens, indexes):
     result = []
     intersect_result = find_token_from_all_indexes(tokens[0], indexes)
+    # looking for all tokens in all indexes
     for token in tokens:
         current_result = find_token_from_all_indexes(token, indexes)
         result.extend(current_result)
         intersect_result = list(set(current_result) & set(intersect_result))
+    # returning a list with all urls with tokens
+    # and a list with urls containing all tokens
     return list(set(result)), intersect_result
 
 
@@ -106,6 +122,7 @@ def verify_tokens_presence(result, intersect_result):
 
 def get_filtered_file(list_of_urls, json_file):
     filtered_file = []
+    # filtration of the json file according to selected urls
     for row in json_file:
         if row.get("url") in list_of_urls:
             filtered_file.append(row)
@@ -127,15 +144,19 @@ def get_list_of_doc_tokens_per_type(filtered_file, list_of_synonyms_indexes):
         url = row["url"]
         title = row["title"]
         description = row["description"]
+
+        # agregation of main features
         features = ""
         for features_name in features_names:
             current_feature = row["product_features"].get(features_name) or ""
             features += " " + current_feature
 
+        # tokenization of each document fields
         title = transform_request(title, list_of_synonyms_indexes)
         description = transform_request(description, list_of_synonyms_indexes)
         features = transform_request(features, list_of_synonyms_indexes)
 
+        # stocking the results of each fields
         title_doc.append({"url": url, "tokens": title})
         description_doc.append({"url": url, "tokens": description})
         features_doc.append({"url": url, "tokens": features})
@@ -144,6 +165,7 @@ def get_list_of_doc_tokens_per_type(filtered_file, list_of_synonyms_indexes):
 
 def merge_list_of_doc_tokens(title_doc, description_doc, features_doc):
     merged_list = []
+    # fusion of all tokenized documents
     for i in range(len(title_doc)):
         tokens = title_doc[i]["tokens"] + description_doc[i]["tokens"] + \
             features_doc[i]["tokens"]
@@ -152,11 +174,13 @@ def merge_list_of_doc_tokens(title_doc, description_doc, features_doc):
 
 
 def get_token_frequence_in_doc_tokens(token, doc_tokens):
+    # computing the frequence of a token in a document field
     tokens = doc_tokens["tokens"]
     return tokens.count(token)
 
 
 def get_token_frequence_in_list_of_doc_tokens(token, list_of_doc_tokens):
+    # computing the frequence of a token in all document firlds
     count = 0
     for doc_tokens in list_of_doc_tokens:
         count += get_token_frequence_in_doc_tokens(token, doc_tokens)
@@ -164,6 +188,7 @@ def get_token_frequence_in_list_of_doc_tokens(token, list_of_doc_tokens):
 
 
 def get_match_count(request_tokens, tokens_list):
+    # compute the number of appearance of tokens in a field
     count = 0
     for token in request_tokens:
         count += tokens_list.count(token)
@@ -175,13 +200,17 @@ def get_match_count(request_tokens, tokens_list):
 def compute_bm25(request_tokens, list_of_doc_tokens, current_doc_tokens,
                  b=0.75, k=1.2):
     result = 0
+    # computing the lengths of field and average fields
     field_len = len(current_doc_tokens["tokens"])
     avg_field_len = np.mean([len(doc_tokens['tokens']) for doc_tokens in
                              list_of_doc_tokens])
     for token in request_tokens:
+        # computing the frequence of the token
         f = get_token_frequence_in_doc_tokens(token, current_doc_tokens)
+        # computing the frequence of the token in the full document
         idf = get_token_frequence_in_list_of_doc_tokens(token,
                                                         list_of_doc_tokens)
+        # computing the BM25 formulae
         numerator = f * (k+1)
         denominator = f + k * (1 - b + b * (field_len/avg_field_len))
         result = idf * numerator / denominator
@@ -193,14 +222,17 @@ def compute_linear_ranking(request_tokens, list_of_merged_docs,
                            current_description_tokens,
                            current_features_tokens):
 
+    # setting weights for the linear ranking
     w_bm25 = 1.0
     w_title = 3.0
     w_description = 0.5
     w_features = 2.0
 
+    # computing the bm25 score
     bm25_score = compute_bm25(request_tokens, list_of_merged_docs,
                               current_doc_tokens, b=0.75, k=1.2)
 
+    # getting the match count for the title, description and features
     title_matches = get_match_count(request_tokens,
                                     current_title_tokens["tokens"])
     features_matches = get_match_count(request_tokens,
@@ -208,6 +240,7 @@ def compute_linear_ranking(request_tokens, list_of_merged_docs,
     desc_matches = get_match_count(request_tokens,
                                    current_features_tokens["tokens"])
 
+    # computing the linear ranking score
     linear_ranking = (bm25_score * w_bm25) + \
                      (title_matches * w_title) + \
                      (desc_matches * w_description) + \
@@ -220,6 +253,7 @@ def compute_ranking_for_all_entries(request_tokens, list_of_doc_tokens,
                                     title_tokens, description_tokens,
                                     features_tokens):
     ranking_result = []
+    # iteration on all documents fields
     for i in range(len(list_of_doc_tokens)):
         current_doc_tokens = list_of_doc_tokens[i]
         current_title_tokens = title_tokens[i]
@@ -227,6 +261,8 @@ def compute_ranking_for_all_entries(request_tokens, list_of_doc_tokens,
         current_features_tokens = features_tokens[i]
 
         url = current_doc_tokens["url"]
+
+        # computing the linear ranking score for the current document
         score = compute_linear_ranking(request_tokens, list_of_doc_tokens,
                                        current_doc_tokens,
                                        current_title_tokens,
@@ -238,7 +274,7 @@ def compute_ranking_for_all_entries(request_tokens, list_of_doc_tokens,
 
 
 def get_ordered_results(ranking_result):
-    # url, score
+    # orderning the results by inverse order score
     sorted_results = sorted(ranking_result, key=lambda x: x['score'],
                             reverse=True)
     return [item['url'] for item in sorted_results]
@@ -250,33 +286,41 @@ def provide_ranking_results(request, document, title_index, description_index,
                             list_of_features_indexes,
                             list_of_synonyms_indexes):
 
+    # loading the query
     request_tokens = transform_request(request, list_of_synonyms_indexes)
 
+    # agregation of all indexes
     all_indexes = [title_index, description_index]
     for feature_list in list_of_features_indexes:
         all_indexes.append(feature_list)
 
+    # finding candidate urls
     res_urls, intersect_urls = find_tokens_from_all_indexes(request_tokens,
                                                             all_indexes)
 
+    # deciding to take the intersection if there are enought results
     if len(intersect_urls) >= 5:
         urls = intersect_urls
     else:
         urls = res_urls
 
+    # getting the filtered document based on the selected urls
     filtered_file = get_filtered_file(urls, document)
 
+    # tokenizing the filtered documents
     title_tokens, description_tokens, features_tokens = \
         get_list_of_doc_tokens_per_type(filtered_file,
                                         list_of_synonyms_indexes)
 
+    # creating a fused document
     merged_tokens = merge_list_of_doc_tokens(title_tokens, description_tokens,
                                              features_tokens)
 
+    # computing the linear ranking score for all documents
     ranking_result = compute_ranking_for_all_entries(request_tokens,
                                                      merged_tokens,
                                                      title_tokens,
                                                      description_tokens,
                                                      features_tokens)
-
+    # returning the ordered urls
     return get_ordered_results(ranking_result)
